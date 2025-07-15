2fa51e15b0d42ddbedad9026672191d639536b97
ShalyginR <roma.shalygin04@mail.ru>
2025-07-15 14:10:20 +0300

    Добавил домашнее задание 10

--- other.py/4.py
+++
@@ -1,9 +0,0 @@
-#касса
-cost = float(input("Введите стоимость товара: "))
-paid = float(input("Введите сумму оплаты: -"))
-
-if paid >= cost:
-   change = paid - cost
-   print ("Сдача:" , change)
-else:
-   print("Недостаточно средств!")
